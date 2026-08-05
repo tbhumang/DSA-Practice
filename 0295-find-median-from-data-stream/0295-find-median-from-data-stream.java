db8b033eafe943bfd255dd29a71ee670b7890546
Umang Tiwari <158751031+tbhumang@users.noreply.github.com>
2026-08-05 19:00:29 +0530

    Time: 219 ms (17.4%), Space: 111.4 MB (54.94%) - LeetHub

---
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.java
@@ -0,0 +1,23 @@
+class MedianFinder {
+    private PriorityQueue<Integer> left; 
+    private PriorityQueue<Integer> right;  
+
+    public MedianFinder() {
+        left = new PriorityQueue<>(Collections.reverseOrder());
+        right = new PriorityQueue<>();
+    }
+    public void addNum(int num) {
+        left.offer(num);
+        right.offer(left.poll());
+        if (right.size() > left.size()) {
+            left.offer(right.poll());
+        }
+    }
+
+    public double findMedian() {
+        if (left.size() > right.size()) {
+            return left.peek();
+        }
+        return (left.peek() + right.peek()) / 2.0;
+    }
+}
