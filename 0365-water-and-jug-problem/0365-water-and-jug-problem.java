74dff2160e32e6d520ad08172b91fe281e985b15
Umang Tiwari <158751031+tbhumang@users.noreply.github.com>
2026-08-07 16:10:56 +0530

    Time: 0 ms (100%), Space: 42.2 MB (43.07%) - LeetHub

---
+++ 0365-water-and-jug-problem/0365-water-and-jug-problem.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean canMeasureWater(int x, int y, int target) {
+        if(target > x + y){
+            return false;
+        }
+        if(target == 0){
+            return true;
+        }
+        return target % gcd(x, y) == 0;
+    }
+    private int gcd(int a, int b){
+        while(b != 0){
+            int temp = a % b;
+            a = b;
+            b = temp;
+        }
+        return a;
+    }
+}
