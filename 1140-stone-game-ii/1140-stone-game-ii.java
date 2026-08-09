a3938a1a18536c725293fab633b0832e5b69e1cb
Umang Tiwari <158751031+tbhumang@users.noreply.github.com>
2026-08-09 16:40:56 +0530

    Time: 7 ms (59.37%), Space: 44.3 MB (74.54%) - LeetHub

---
+++ 1140-stone-game-ii/1140-stone-game-ii.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int stoneGameII(int[] piles) {
+        int n = piles.length;
+        int[] suffix = new int[n + 1];
+
+        for (int i = n - 1; i >= 0; i--) {
+            suffix[i] = suffix[i + 1] + piles[i];
+        }
+        int[][] dp = new int[n + 1][n + 1];
+        for (int i = n - 1; i >= 0; i--) {
+
+            for (int m = 1; m <= n; m++) {
+                if (i + 2 * m >= n) {
+                    dp[i][m] = suffix[i];
+                    continue;
+                }
+                int best = 0;
+                for (int x = 1; x <= 2 * m && i + x <= n; x++) {
+
+                    int newM = Math.max(m, x);
+                    int current = suffix[i] - dp[i + x][newM];
+
+                    best = Math.max(best, current);
+                }
+
+                dp[i][m] = best;
+            }
+        }
+        return dp[0][1];
+    }
+}
