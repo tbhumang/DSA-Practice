dd548276fb13a00ad683890327ce359493575bdf
Umang Tiwari <158751031+tbhumang@users.noreply.github.com>
2026-08-21 16:54:26 +0530

    Time: 3 ms (12.92%), Space: 42.6 MB (61.35%) - LeetHub

---
+++ 0377-combination-sum-iv/0377-combination-sum-iv.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int combinationSum4(int[] nums, int target) {
+        int[] dp = new int[target + 1];
+
+        dp[0] = 1;
+        for(int sum = 1; sum  <= target; sum++){
+            for(int num : nums){
+                if(num <= sum){
+                    dp[sum] += dp[sum - num];
+                }
+            }
+        }
+        return dp[target];
+    }
+}
